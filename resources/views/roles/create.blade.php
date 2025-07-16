5f9e87a383a7f18995d9345ecfd34534c5964c56
Ranjeet225 <ranjeetmaurya2033@gmail.com>
2025-07-16 23:29:54 +0530

    data

--- resources/views/roles/create.blade.php
+++ resources/views/roles/create.blade.php
@@ -10,14 +10,15 @@
             <input type="text" name="name" class="form-control" value="{{ old('name') }}" required>
             @error('name')<div class="text-danger">{{ $message }}</div>@enderror
         </div>
-        <div class="mb-3">
-            <label for="permissions" class="form-label">Permissions</label>
-            @foreach($permissions as $permission)
-                <div>
-                    <input type="checkbox" name="permissions[]" value="{{ $permission->id }}" 
-                        {{ old('permissions') && in_array($permission->id, old('permissions', [])) ? 'checked' : '' }}>
-                    <label for="permission-{{ $permission->id }}">{{ $permission->name }}</label>
-                </div>  
+        <div class="mb-3 d-flex flex-wrap">
+            @foreach($permissions->chunk(4) as $permissionChunk)
+                @foreach($permissionChunk as $permission)
+                    <div class="form-check form-check-inline mr-3">
+                        <input class="form-check-input" type="checkbox" name="permissions[]" value="{{ $permission->id }}" 
+                            {{ old('permissions') && in_array($permission->id, old('permissions', [])) ? 'checked' : '' }}>
+                        <label class="form-check-label" for="permission-{{ $permission->id }}">{{ $permission->name }}</label>
+                    </div>
+                @endforeach
             @endforeach
         </div>
         <button type="submit" class="btn btn-primary">Create</button>
